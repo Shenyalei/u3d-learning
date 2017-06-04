0e953dbc522ec2cf6d327bf594df9d8d734b015b
Summer <529854057@qq.com>
2017-06-04 23:09:30 +0800

    move test

---
+++ move test/Assets/MoveObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveObject : MonoBehaviour {
+
+    public float speed = 5.0f;
+    private Rigidbody2D rb2D;
+    // Use this for initialization
+    protected virtual void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveToPos(Vector3 pos)
+    {
+        StopCoroutine("SmoothMovement");
+        StartCoroutine("SmoothMovement", pos);
+    }
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, speed * Time.deltaTime);
+            rb2D.MovePosition(newPostion);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+}
---
+++ move test/Assets/player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MoveObject {
+
+    public float minDelay = 0.2f;
+    public float maxDelay = 1.0f;
+    public RemotePlayer remotePlayer;
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+            Vector3 mousePositionOnScreen = Input.mousePosition;
+            mousePositionOnScreen.z = screenPosition.z;
+            Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionOnScreen);
+            MoveToPos(mousePositionInWorld);
+            float delay = Random.Range(minDelay, maxDelay);
+            StartCoroutine(SendMsg(delay,mousePositionInWorld));
+        }
+     }
+
+    protected IEnumerator SendMsg(float delayTime,Vector3 pos)
+    {
+        yield return new WaitForSeconds(delayTime);
+        remotePlayer.MoveToPos(pos);
+    }
+}
